2ba47944a0534a80a55857ee4f3e901037aa71c0
wrxth <60602762+wrxth@users.noreply.github.com>
2021-11-10 16:20:20 +0100

    comments

--- Pirate game/WaterManager.cs
+++ Pirate game/WaterManager.cs
@@ -14,13 +14,14 @@ public class WaterManager : MonoBehaviour
     private void Update()
     {
         Vector3[] vertices = Mesh.mesh.vertices;
-
+        
+        // Bepaal de nieuwe positie voor de vertices
         for (int i = 0; i < vertices.Length; i++)
         {
             vertices[i].y = WaveManager.instance.GetWaveHeight(transform.position.x + vertices[i].x);
         }
-
-
+        
+        // Zet de vertices goed
         Mesh.mesh.vertices = vertices;
         Mesh.mesh.RecalculateNormals();
     }
